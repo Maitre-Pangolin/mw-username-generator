62af8090db9af2cfb460411d4c24c3936131ec85
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-05-11 11:42:31 -0400

    Add name generation

--- namegenerator.js
+++ namegenerator.js
@@ -1,1 +1,12 @@
-module.exports = () => Math.floor(Math.random() * 100);
+const { adjective, noun } = require("./res/words");
+
+function randArr(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function firstToUpper(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+module.exports = () =>
+  firstToUpper(randArr(adjective)) + firstToUpper(randArr(noun));
